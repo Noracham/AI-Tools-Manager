65d01ae604770a1e3f0f65317b87d6b8da59dd92
Noranosan <nsmrym04@gmail.com>
2024-11-23 12:12:51 +0900

    fix:outで環境変数をnetlifyにenv登録

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN: 'aitoolsmanager',
-    NEXT_PUBLIC_MICROCMS_API_KEY: 'PsUD5sZDHJJJdkXAW0mDhkXnsDQQlJGmYC5s',
-  }
+module.exports = {
+  exportPath: 'out',
 };
-
-module.exports = nextConfig;
